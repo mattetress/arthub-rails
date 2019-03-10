feb3a80fc2a27df85b5df595e7a1cc9ea7dc0576
mattetress <mattetress@me.com>
2019-03-09 18:59:32 -0600

    sanitize user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,10 @@ class UsersController < ApplicationController
       render 'users#new'
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :bio, :website)
+  end 
 end
