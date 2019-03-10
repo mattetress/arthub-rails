a7caf6f6a8c63bb152c929e0bf3da001e84c4c83
mattetress <mattetress@me.com>
2019-03-09 19:57:30 -0600

    create  log in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      flash[:error] = "There was a problem logging you in. Please check credentials and try again."
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
 end
