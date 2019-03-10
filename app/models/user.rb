bfe58b00a528822cd027495fbaae37f4a1c435cd
mattetress <mattetress@me.com>
2019-03-09 19:05:26 -0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_one_attached :avatar
+
+  validates :email, uniqueness: :true,
+                    presence: :true
+  validates :password, presence: :true
 end
