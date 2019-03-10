974fd56e92a96fc0e0a9df98c8dc24e848f5d4fe
mattetress <mattetress@me.com>
2019-03-09 19:35:43 -0600

    add ability to upload profile photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end 
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :bio, :website)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :bio, :website, :avatar)
   end
 end
