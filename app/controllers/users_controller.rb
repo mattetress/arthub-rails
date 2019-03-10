84aee32afc90351c08f941c269a12b09235b9c39
mattetress <mattetress@me.com>
2019-03-09 18:37:04 -0600

    add users/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      render :create
+      render 'users#new'
     end
-  end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static#home'
-  resources :session
-  resources :user 
+  resources :sessions, :only => [:new, :create, :destroy]
+  resources :users
 
 end
