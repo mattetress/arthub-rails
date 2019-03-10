afef6fe12f65ea0c91418086e2adebae44fcd9e7
mattetress <mattetress@me.com>
2019-03-09 18:33:45 -0600

    define logged_in? and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    user ||= User.find(session[:user_id])
+  end
 end
