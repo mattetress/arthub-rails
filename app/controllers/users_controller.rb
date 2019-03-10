07f2910312c818be573caccec5fc37acd75f5727
mattetress <mattetress@me.com>
2019-03-09 18:13:15 -0600

    generate static controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :create
+    end
+  end 
 end
