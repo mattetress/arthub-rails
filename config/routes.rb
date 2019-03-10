9a889608207d4620ce52c9c1b58254510faed081
mattetress <mattetress@me.com>
2019-03-09 18:23:50 -0600

    add static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static#home'
+  resources :session
+  resources :user 
+
 end
