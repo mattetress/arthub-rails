026648b1e18e84c4d2d7660a66741caa5ba95d91
mattetress <mattetress@me.com>
2019-03-09 19:22:35 -0600

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,17 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      render 'users#new'
+      render :new
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end 
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :bio, :website)
-  end 
+  end
 end
