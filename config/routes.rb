0c153154915a480996cc749c4a2f06cad3cbf7b4
mattetress <mattetress@me.com>
2019-03-09 18:43:12 -0600

    edit routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static#home'
+  get '/signup' => 'users#new', as: 'new_user'
+  post '/signup' => 'users#create', as: 'users'
   resources :sessions, :only => [:new, :create, :destroy]
-  resources :users
+  resources :users, except: [:new, :create]
 
 end
