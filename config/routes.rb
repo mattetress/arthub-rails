c4341b103943dd64c417efc00b7e403c0075c25f
mattetress <mattetress@me.com>
2019-03-14 16:21:25 -0500

    add nested resource for user artworks

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,25 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static#home'
+
   get '/signup' => 'users#new', as: 'new_user'
+
   post '/signup' => 'users#create', as: 'users'
+
   get '/login' => 'sessions#new'
+
   post '/login' => 'sessions#create'
+
   delete '/logout' => 'sessions#destroy'
+
   get '/auth/facebook/callback' => 'sessions#facebook'
-  resources :users, except: [:new, :create]
+
+  resources :users, except: [:new, :create] do
+    resources :artworks
+  end
+
   get '/users/:id/update_photo' => 'users#change_avatar', as: 'edit_avatar'
+
   post '/users/:id/update_photo' => 'users#update_avatar'
 
 end
