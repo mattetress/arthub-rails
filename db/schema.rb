004ffbe17147039fa68f6c7044ed57c2342e701c
mattetress <mattetress@me.com>
2019-03-13 21:45:39 -0500

    add user_id and url to artworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_180653) do
+ActiveRecord::Schema.define(version: 2019_03_14_024412) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_03_10_180653) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
