5cb9a53f1e4eff5358bb4a758b4bb6e7f92609e9
mattetress <mattetress@me.com>
2019-03-15 22:31:50 -0500

    add relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_one_attached :resume
   has_many :artworks
+  has_many :events
 
   validates :email, uniqueness: :true,
                     presence: :true
