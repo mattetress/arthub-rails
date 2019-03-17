48dd095d99af76d66ed369aa09701d5538713388
mattetress <mattetress@me.com>
2019-03-16 19:26:01 -0500

    add new and create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,17 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to @event
+    else
+      render 'events/new'
+    end 
+  end
+
 end
