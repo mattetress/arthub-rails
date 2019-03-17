fcdca0930c7955fd24f6b96103dcaaf0f70a543f
mattetress <mattetress@me.com>
2019-03-17 17:56:31 -0500

    fix issue authorizing admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,7 +6,9 @@ RailsAdmin.config do |config|
   # config.authenticate_with do
   #   warden.authenticate! scope: :user
   # end
-  # config.current_user_method(&:current_user)
+  config.parent_controller = '::ApplicationController'
+  
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
