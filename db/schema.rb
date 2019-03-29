73e1fb0a663fc6304ac309b9185fc3640815fe31
mattetress <mattetress@me.com>
2019-03-29 14:51:16 -0500

    add venue to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_193310) do
+ActiveRecord::Schema.define(version: 2019_03_29_195018) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_03_29_193310) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "venue"
   end
 
   create_table "interests", force: :cascade do |t|
