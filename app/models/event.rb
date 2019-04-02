1680e7253c0187e64492af6b85139e47bb4386b8
mattetress <mattetress@me.com>
2019-04-02 17:39:02 -0500

    fix error when updating event location

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,9 +4,9 @@ class Event < ApplicationRecord
   has_many :interests
   has_many :comments
   has_many :users, through: :interests
-  has_one :location
+  has_one :location, dependent: :destroy
   has_one :city, through: :location
-  accepts_nested_attributes_for :location 
+  accepts_nested_attributes_for :location
 
   def self.past_events
     self.where("end_time < ?", Time.now).order('end_time desc')
