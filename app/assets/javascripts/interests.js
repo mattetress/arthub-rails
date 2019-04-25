8d5465e7b5382b3f826b9f4e16dfcc24736c660c
mattetress <mattetress@me.com>
2019-04-25 16:46:13 -0500

    handle star color on hover

--- app/assets/javascripts/interests.js
+++ app/assets/javascripts/interests.js
@@ -8,21 +8,37 @@ function attachStarListener(star) {
   $(star).on('click', function(){
     toggleInterest(star);
   })
+
+  $(star).hover(function (){
+    toggleStarImage(star);
+    toggleState(star);
+  }, () => {
+    toggleStarImage(star);
+    toggleState(star);
+  })
 }
 
 function toggleInterest(star) {
   const id = parseInt($(star).data("id"));
   const state = parseInt($(star).data("state"));
   $.get(`/events/${id}/toggle_interest`);
-  toggleStarImage(star, state);
+  toggleState(star);
 }
 
-function toggleStarImage(star, state) {
+function toggleStarImage(star) {
+  const state = parseInt($(star).data("state"));
   if (state) {
     $(star).attr('src', '/assets/star-blank32.png');
-    $(star).data("state", "0");
   } else {
     $(star).attr("src", '/assets/star32.png');
+  }
+}
+
+function toggleState(star) {
+  const state = parseInt($(star).data("state"));
+  if (state) {
+    $(star).data("state", "0");
+  } else {
     $(star).data("state", "1");
   }
 }
