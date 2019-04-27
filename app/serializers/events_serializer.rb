ec537f908d0cc357577effd2dfbd6c59817068ba
mattetress <mattetress@me.com>
2019-04-27 18:00:56 -0500

    change json results

--- app/serializers/events_serializer.rb
+++ app/serializers/events_serializer.rb
@@ -1,12 +1,22 @@
 class EventsSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :image_url, :start_time, :end_time, :venue, :accepting_applications
+  
+  attributes :id, :user_id, :name, :description, :image_url, :start_time, :end_time, :venue, :accepting_applications, :user_count
   has_many :users
+  belongs_to :owner
 
   def image_url
     if object.image.attached?
       variant = object.image.variant(resize: "150x150")
-      return rails_representation_url(variant, only_path: true)
+      rails_representation_url(variant, only_path: true)
     end
+  end
+
+  def user_count
+    object.users.count
+  end
+
+  def owner
+    object.user
   end
 end
