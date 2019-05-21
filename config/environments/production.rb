cab62e0ebba35becbe95db18d5f8b3aca011c96b
mattetress <mattetress@me.com>
2019-05-21 12:53:57 -0500

    add asset configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+# Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+# Generate digests for assets URLs
+  config.assets.digest = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
