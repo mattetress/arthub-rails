8bfdf133bc0fd9685092565f4a2b1cdc699e58c7
mattetress <mattetress@me.com>
2019-05-21 20:33:01 -0500

    configure uid to be bigint for facebook oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_195018) do
+ActiveRecord::Schema.define(version: 2019_05_22_012930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2019_03_29_195018) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "uid"
+    t.bigint "uid"
     t.boolean "admin", default: false
   end
 
